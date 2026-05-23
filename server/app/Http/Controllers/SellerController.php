e00e7c9f881f40e6b20602638dbdec7aba6e283f
Kittienessless123 <paranina.thebell@gmail.com>
2026-05-23 22:46:53 +0700

    updated : seller  service and controller

--- server/app/Http/Controllers/SellerController.php
+++ server/app/Http/Controllers/SellerController.php
@@ -3,20 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Settings\ProfileDeleteRequest;
-use App\Http\Requests\Settings\ProfileUpdateRequest;
+use App\Http\Requests\Seller\SellerCreationRequest;
+use App\Http\Requests\Seller\SellerUpdateRequest;
+use App\Http\Requests\Seller\SellerDeleteManyRequest;
+use App\Http\Requests\Seller\SellerDeleteRequest;
+use App\Http\Services\SellerService;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\View\View;
 
 class SellerController extends Controller
 {
-  public function add() {}
+  public function __construct(
+    private SellerService $sellerService
+  ) {}
+  public function index(Request $request): JsonResponse
+  {
+    $sellers = $this->sellerService->getAll();
+    return response()->json($sellers);
+  }
 
-  public function edit() {}
-  public function update() {}
-  public function destroy() {}
+  public function store(SellerCreationRequest $request): JsonResponse
+  {
+    $seller = $this->sellerService->create($request->validated());
+    return response()->json($seller, 201);
+  }
+
+  public function update(SellerUpdateRequest $request, string $id): JsonResponse
+  {
+    $data = $request->validated();
+    $seller = $this->sellerService->update($id, $data);
+
+    if (!$seller) {
+      return response()->json([
+        'success' => false,
+        'message' => 'Seller not found'
+      ], 404);
+    }
+
+    return response()->json($seller);
+  }
+
+  public function destroy(SellerDeleteRequest $request, string $id): JsonResponse
+  {
+    $data = $request->validated();
+
+    $deleted = $this->sellerService->delete($data);
+
+    if (!$deleted) {
+      return response()->json([
+        'success' => false,
+        'message' => 'Seller not found'
+      ], 404);
+    }
+
+    return response()->json(null, 204);
+  }
+
+  public function showView(string $id): View
+  {
+    $seller = $this->sellerService->getOneById($id);
+
+    return view('seller.view', [
+      'seller' => $seller
+    ]);
+  }
 }
--- server/app/Services/SellerService.php
+++ server/app/Services/SellerService.php
@@ -9,12 +9,15 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class SellerService extends BaseService
 {
-  protected SellerRepository $repo; // Теперь работает!
+  protected SellerRepository $repo;
 
   public function __construct(SellerRepository $repo)
   {
     parent::__construct($repo);
   }
 
-  
+  public function getSellerByNamePaginated(string $name, int $perPage = 15): LengthAwarePaginator
+  {
+    return $this->repo->getSellerByNamePaginated($name, $perPage);
+  }
 }
